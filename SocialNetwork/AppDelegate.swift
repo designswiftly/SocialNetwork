5b2109c279b3696bcc48f31601a03298a0fb4db8
Pierluigi Cifani <piercifani@gmail.com>
2014-11-26 17:41:31 +0100

    Finish off

--- SocialNetwork/AppDelegate.swift
+++ SocialNetwork/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UITextField.appearance().tintColor = UIColor.whiteColor()
+        UITextField.appearance().keyboardAppearance = .Dark
+
         return true
     }
 
--- SocialNetwork/UserPasswordViewController.swift
+++ SocialNetwork/UserPasswordViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class UserPasswordViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBAction func onDismiss(sender: AnyObject) {
+        self.emailTextField.resignFirstResponder()
+        self.passwordTextField.resignFirstResponder()
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Preparar Botón
+        loginButton.layer.cornerRadius = 15
+        loginButton.clipsToBounds = true
+        loginButton.titleLabel?.font = UIFont(name: "BAUER", size: 24.0)
+        
+        //Mostrar el teclado
+        self.emailTextField.becomeFirstResponder()
     }
 }
